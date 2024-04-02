7f46b613c8445e99c88c5ce1c0c4bded9dee6feb
Your Name <you@example.com>
2024-04-02 18:10:47 +0200

    redirect to 404-NotFound page OK

---
+++ src/components/pages/NotFoundPage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: '404: Not-Found',
+}
+</script>
+
+<template>
+    <h1 class="text-center">404 Page Not Found</h1>
+    <RouterLink to="/">Torna alla Homepage</RouterLink>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-export { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import HomePage from '../components/pages/HomePage.vue';
 import ContactUsPage from '../components/pages/ContactUsPage.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import NotFoundPage from '../components/pages/NotFoundPage.vue';
 
 export default createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: HomePage },
         { path: '/contact-us', component: ContactUsPage },
-        { path: '/pathMatch(.*)*', redirect: '/' },
+        { path: '/:pathMatch(.*)*', component: NotFoundPage },
     ]
 })
