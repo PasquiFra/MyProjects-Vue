32f4e94719b287471365c69b68649e212954d9c1
Your Name <you@example.com>
2024-04-04 16:06:52 +0200

    send email form created

--- src/components/pages/ProfilePage.vue
+++ src/components/pages/ProfilePage.vue
@@ -5,7 +5,22 @@ export default {
 </script>
 
 <template>
-    <h1>Contattami</h1>
+    <h1 class="text-center my-3">Contattami</h1>
+
+    <form @submit.prevent="">
+
+        <div class="mb-3">
+            <label for="email_send" class="form-label">Indirizzo Email</label>
+            <input type="email" class="form-control" id="email_send" placeholder="mario@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="text_send" class="form-label">Invia un messaggio:</label>
+            <textarea class="form-control" id="text_send" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mb-3">Invia richiesta</button>
+
+    </form>
+
 </template>
 
 <style lang="scss" scoped></style>
